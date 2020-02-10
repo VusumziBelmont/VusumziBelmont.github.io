4d8f1d186e3eb10221f120e0e5d06037fe95f229
Vusumzi Belmont <vusumzibelmontit@gmail.com>
2020-02-10 11:56:18 -0600

    added new web site

---
+++ precache-manifest.8636848297988a7a270e0d1a05130d8b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0267bb3f6b797328ab5e8ed34e9662e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ae399aa33d085e03ec",
+    "url": "/static/css/main.9abdd7ce.chunk.css"
+  },
+  {
+    "revision": "9ab6426015db07e69653",
+    "url": "/static/js/2.d921312d.chunk.js"
+  },
+  {
+    "revision": "33ae399aa33d085e03ec",
+    "url": "/static/js/main.bc7228da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d74c3c2d20e8d92e894185201cd6c383",
+    "url": "/static/media/encoding.d74c3c2d.svg"
+  },
+  {
+    "revision": "19778cf9c33d0c98076aeffd7dd100aa",
+    "url": "/static/media/font-mfizz.19778cf9.ttf"
+  },
+  {
+    "revision": "4a873e5f62afe3c8a79b6250b3d477db",
+    "url": "/static/media/font-mfizz.4a873e5f.svg"
+  },
+  {
+    "revision": "4d03e427f13a893d5e9ce14ff5ed8287",
+    "url": "/static/media/font-mfizz.4d03e427.eot"
+  },
+  {
+    "revision": "64c7cf6cb98b1297cd522df93db3d0d2",
+    "url": "/static/media/font-mfizz.64c7cf6c.woff"
+  },
+  {
+    "revision": "1b7af7f92ee653a284b14d32d6b745b4",
+    "url": "/static/media/logofinal.1b7af7f9.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "6ff604e5ddc3f2154d33e374e257e5f4",
+    "url": "/static/media/scenic-view.6ff604e5.jpg"
+  }
+]);
